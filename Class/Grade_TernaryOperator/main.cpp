d747670a00f9b73d92cadf09a93fe486f9180fd7
gmwideout <gmwideout91@gmail.com>
2017-02-26 22:38:03 -0800

    Add files via upload

---
+++ Class/Grade_TernaryOperator/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Gavin Mullins
+ * Created on January 12,2017, 11:18 AM
+ * Purpose:  Grade Ternary Operator
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    unsigned short score;
+    char grade;
+    
+    //Input or initialize values Here
+    cout<<"This program takes the score and ouputs the grade"<<endl;
+    cout<<"Input the score 0 to 100"<<endl;
+    cin>>score;
+    
+    //Process/Calculations Here
+    grade=(score>=90)?'A':
+          (score>=80)?'B':
+          (score>=70)?'C':
+          (score>=60)?'D':'F';
+    
+    //Output Located Here
+    cout<<"Your score of "<<score<<" = "<<grade<<endl;
+
+    //Exit
+    return 0;
+}
+
